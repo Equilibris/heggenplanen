c02ae26ce49e433fde7289b1901a624463b12c2b
RogueMacro <william.tetlie@outlook.com>
2021-09-27 22:21:46 +0200

    assignment page prototype

--- pages/assignments.tsx
+++ pages/assignments.tsx
@@ -16,6 +16,68 @@ import format from 'date-fns/format'
 import formatDistance from 'date-fns/formatDistance'
 import nbLocale from 'date-fns/locale/nb'
 
+// FROM STACKOVERFLOW
+function isDateInWeek(date: Date, offset = 0) {
+	const todayObj = new Date()
+	const todayDate = todayObj.getDate()
+	const todayDay = todayObj.getDay()
+
+	// get first date of week
+	const firstDayOfWeek = new Date(
+		todayObj.setDate(todayDate - todayDay + 7 * offset),
+	)
+
+	// get last date of week
+	const lastDayOfWeek = new Date(firstDayOfWeek)
+	lastDayOfWeek.setDate(lastDayOfWeek.getDate() + 6 + 7 * offset)
+
+	// if date is equal or within the first and last dates of the week
+	return date >= firstDayOfWeek && date <= lastDayOfWeek
+}
+
+type AssignmentStackProps = {
+	label: string
+	assignments: [string, AssignmentBlock][]
+}
+
+const AssignmentStack = ({ label, assignments }: AssignmentStackProps) => {
+	return (
+		<div>
+			<Typography variant='h4'>
+				<PrimaryBox>{label}</PrimaryBox>
+			</Typography>
+			<br />
+			<Stack flexDirection='row'>
+				{assignments.map(([key, value]) => (
+					<Accordion key={key}>
+						<AccordionSummary
+							expandIcon={
+								<PrimaryBox>
+									<ExpandMoreIcon color='inherit' />
+								</PrimaryBox>
+							}>
+							<Typography>
+								<Box>{value.name}</Box>
+								<DisabledBox>
+									{
+										// formatDistance(new Date(), value.due) +
+										format(value.due, 'EEEE d/M, hh:MM', {
+											locale: nbLocale,
+										})
+									}
+								</DisabledBox>
+							</Typography>
+						</AccordionSummary>
+						<AccordionDetails>
+							<Typography>{value.message}</Typography>
+						</AccordionDetails>
+					</Accordion>
+				))}
+			</Stack>
+		</div>
+	)
+}
+
 const Assignments = () => {
 	const [assignmentData] = useAssignmentData()
 
@@ -25,6 +87,18 @@ const Assignments = () => {
 		) as [string, AssignmentBlock][]
 	).sort((a, b) => compareAsc(a[1].due, b[1].due))
 
+	const thisWeekAssignments = assignments.filter(([_, value]) =>
+		isDateInWeek(value.due),
+	)
+
+	const nextWeekAssignments = assignments.filter(([_, value]) =>
+		isDateInWeek(value.due, 1),
+	)
+
+	const futureAssignments = assignments.filter(
+		([_, value]) => !isDateInWeek(value.due) && !isDateInWeek(value.due, 1),
+	)
+
 	return (
 		<>
 			<Head>
@@ -32,36 +106,9 @@ const Assignments = () => {
 			</Head>
 
 			<MainContainer>
-				<Typography variant='h4'>
-					<PrimaryBox>Denne uken</PrimaryBox>
-				</Typography>
-				<Stack flexDirection='column'>
-					{assignments.map(([key, value]) => (
-						<Accordion key={key}>
-							<AccordionSummary
-								expandIcon={
-									<PrimaryBox>
-										<ExpandMoreIcon color='inherit' />
-									</PrimaryBox>
-								}>
-								<Typography>
-									<Box>{value.name}</Box>
-									<DisabledBox>
-										{
-											// formatDistance(new Date(), value.due) +
-											format(value.due, 'EEEE d/M, hh:MM', {
-												locale: nbLocale,
-											})
-										}
-									</DisabledBox>
-								</Typography>
-							</AccordionSummary>
-							<AccordionDetails>
-								<Typography>{value.message}</Typography>
-							</AccordionDetails>
-						</Accordion>
-					))}
-				</Stack>
+				<AssignmentStack label='Denne uken' assignments={thisWeekAssignments} />
+				<AssignmentStack label='Neste uke' assignments={nextWeekAssignments} />
+				<AssignmentStack label='Fremtidige' assignments={futureAssignments} />
 			</MainContainer>
 		</>
 	)
@@ -70,10 +117,10 @@ const Assignments = () => {
 const MainContainer = styled.div`
 	display: flex;
 	flex-direction: column;
-	align-items: center;
-	justify-content: center;
+	gap: 50px;
 	min-height: calc(100vh - 12rem);
-	width: 100vw;
+	width: 94vw;
+	padding: 3vw;
 	padding-block-start: 6rem;
 	padding-block-end: 6rem;
 `
